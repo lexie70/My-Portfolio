66c1e51d8804d05671755137ed26b7130f9715b0
lexie70 <alexandracampos658@gmail.com>
2023-05-19 14:06:36 -0600

    primer commit

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+import RootLayout from "./layout";
+
+export default function Home() {
+  return (<>
+ <div className="container W-100">
+    <div className="row w-50">
+      <div className="col-12">ABOUT</div>
+    </div>
+    <div className="row w-50">
+      <div className="col-12">PROJECTS</div>
+    </div>
+    <div className="row w-50">
+      <div className="col-12">CONTACT</div>
+    </div>
+ </div>
+  </>
+  )
+}
+
---
+++ src/app/segundaCosa.jsx
@@ -0,0 +1,7 @@
+
+export default function SegundaCosa() {
+    return (
+      <h1>segunda cosa</h1>
+    )
+  }
+  
