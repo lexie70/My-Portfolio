3fc0e96d3e274c498f8b3ca990cf15e431fbb737
lexie70 <alexandracampos658@gmail.com>
2023-05-29 13:22:11 -0600

    helvetica font imported to global

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,10 +10,10 @@ export default function Home() {
  <div>
     <div className="developer">Developer</div>
 </div>
-<div>
+<div className=" container">
   <h5>Alexandra Campos</h5>
-  <h1>Frontend Developer and web designer</h1>
-  <p>Over the past 5 years I have been working with big eCommerce companies around the world. Offering services around them, working solo, leading small eCommerce teams, maintaining eCommerce websites and developing SAAS</p>
+  <h1 className="col-6">Frontend Developer and web designer</h1>
+  <p className="col-7">Over the past 5 years I have been working with big eCommerce companies around the world. Offering services around them, working solo, leading small eCommerce teams, maintaining eCommerce websites and developing SAAS</p>
 </div>
   </>
   )
