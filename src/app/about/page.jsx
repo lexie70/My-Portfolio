137814ee741746e92e2113423be653d5e650e006
lexie70 <alexandracampos658@gmail.com>
2023-06-19 14:38:09 -0600

    carousel component pending test functionality

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,20 @@
 import Link from "next/link"
+import Image from "next/image"
+import flores from "../../../public/img/flores.jpg"
+import Carousel from "../components/Carousel"
+
 export default function About() {
   return (<>
-  <h1 className="">Este es es about me</h1>
+  <div className="mobile large-screen mainBox-about">
+    <h5 className="mb-4">A FEW WORDS ABOUT ME</h5>
+    <h1>I'm Alexandra Campos, a multidisciplinary developer who focuses on telling my clients’ stories visually, through enjoyable
+       and meaningful experiences. I specialize in responsive websites and functional user interfaces.</h1>
+    <h3 className="mt-5 h3class-about">Over the past 3 years I have been working with eCommerce companies around the world and have been offering services around them,
+       maintaining eCommerce websites and developing SAAS.
+     In my spare time I enjoy exercising and playing videogames.</h3>
+  </div>
+  <Carousel/>
   </>
   )
 }
+
---
+++ src/app/components/Carousel.jsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import flores from "../../../public/img/flores.jpg"
+
+export default function Carousel(){
+    return<>
+        
+<div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+    <div className="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+
+    <div className="carousel-inner mobile large-screen">
+        {/* Este es el primer slider del carrusel */}
+        <div className="carousel-item active" data-bs-interval="10000">
+            <div className="d-flex justify-content-center">
+                <Image 
+                    src={flores}
+                    className="d-block w-100" 
+                    alt="..."
+                    width={500}
+                    height={700}
+                    style={{maxWidth:"100%",height:"500px"}}
+                />
+            </div>
+        </div>
+         {/* Este es el segundo slider del carrusel */}
+        <div className="carousel-item">
+            <div className="d-flex justify-content-center">
+                <Image 
+                    src={flores} 
+                    className="d-block w-100" 
+                    alt="..." 
+                    width={500}
+                    height={700}
+                />
+            </div>
+        </div>
+            
+         {/* Este es el tercer slider del carrusel */}
+        <div className="carousel-item bg-dark">
+            <div className="d-flex justify-content-center">
+                <Image 
+                    src={flores}
+                    className="d-block w-100" 
+                    alt="..."
+                    width={901}
+                    height={700}
+                />
+            </div>
+        </div>
+    </div>
+
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+</div>
+    </>
+}
+  
+    
+    
+    
