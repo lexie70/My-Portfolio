e177269cf4a3977efbbd5756aeeb92dee66d2192
lexie70 <alexandracampos658@gmail.com>
2023-06-27 18:30:48 -0600

    deploy test commit #2 Bootstrap link & script

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,8 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <title>My first next 13</title>
+        <title>Alexandra's Portfolio</title>
+        <link precedence="default" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"/>
       </head>
       <body>
             <Navbar/>
@@ -16,6 +17,8 @@ export default function RootLayout({ children }) {
               {children}
           </div>
         <Footer/>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossOrigin="anonymous"></script>
       </body>
     </html>  
   )
